6a38d344f93d824026cffd5e12038e0ba0810dde
Travis CI <amishshah.2k@gmail.com>
2017-09-10 15:30:27 +0000

    Webpack build for branch master: 98b81fac3812bb1f84c83d75aaa96b250bdbef70

--- discord.master.js
+++ discord.master.js
@@ -3833,7 +3833,7 @@ class Permissions {
     let total = 0;
     for (let p = permissions.length - 1; p >= 0; p--) {
       const perm = this.constructor.resolve(permissions[p]);
-      if ((this.bitfield & perm) !== perm) total |= perm;
+      total |= perm;
     }
     if (Object.isFrozen(this)) return new this.constructor(this.bitfield | total);
     this.bitfield |= total;
@@ -3849,7 +3849,7 @@ class Permissions {
     let total = 0;
     for (let p = permissions.length - 1; p >= 0; p--) {
       const perm = this.constructor.resolve(permissions[p]);
-      if ((this.bitfield & perm) === perm) total |= perm;
+      total |= perm;
     }
     if (Object.isFrozen(this)) return new this.constructor(this.bitfield & ~total);
     this.bitfield &= ~total;
