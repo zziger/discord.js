b3ebd0c2a30b29623a57f770ee15328be239b501
Travis CI <amishshah.2k@gmail.com>
2018-02-01 19:09:33 +0000

    Webpack build for branch 11.3-dev: 1f14758e0c29f2ed6dfd5d920b3efc512ae39cb1

--- discord.11.3-dev.js
+++ discord.11.3-dev.js
@@ -15131,7 +15131,8 @@ class ClientUser extends User {
 
     /**
      * The email of this account
-     * @type {string}
+     * <warn>This is only filled when using a user account.</warn>
+     * @type {?string}
      */
     this.email = data.email;
     this.localPresence = {};
